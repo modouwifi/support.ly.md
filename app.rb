81b663807b21e40313176e5001ccbba2d399c35b
Forrest Ye <afu@forresty.com>
2014-07-23 13:41:55 +0800

    add gauges script

--- app.rb
+++ app.rb
@@ -9,12 +9,14 @@ end
 get '/' do
   template = File.read(File.expand_path("../template.html.erb", __FILE__))
   renderer = ERB.new(template)
+
   renderer.result
 end
 
 post '/support' do
   api_key = ENV["POSTMARK_API_KEY"]
   client = Postmark::ApiClient.new(api_key)
+
   client.deliver(from: 'cs@mochui.net',
                  to: 'afu@forresty.com',
                  subject: "support request from #{params['name']}",
