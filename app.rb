4a611ffcaa982e60b2e63d510fe64e61eda3b8a1
Forrest Ye <afu@forresty.com>
2014-07-23 11:34:30 +0800

    add template

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require "sinatra"
+require "erb"
 
 configure :production do
   require "newrelic_rpm"
 end
 
 get '/' do
-  "hello world"
+  template = File.read(File.expand_path("../template.html.erb", __FILE__))
+  renderer = ERB.new(template)
+  renderer.result
+end
+
+post '/support' do
+  p params
+  'thanks'
 end
