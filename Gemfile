87ddde9a00f80c45085d0f96f9762bb4c06990ca
Forrest Ye <afu@forresty.com>
2014-07-23 12:00:16 +0800

    send email

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.0.0'
 
 gem 'newrelic_rpm'
 gem 'sinatra'
+gem 'postmark'
 
 group :development do
   gem 'shotgun'
--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "erb"
+require "postmark"
 
 configure :production do
   require "newrelic_rpm"
@@ -13,5 +14,13 @@ end
 
 post '/support' do
   p params
+
+  api_key = ENV["POSTMARK_API_KEY"]
+  client = Postmark::ApiClient.new(api_key)
+  client.deliver(from: 'cs@mochui.net',
+                 to: 'afu@forresty.com',
+                 subject: "support request from #{params['name']}",
+                 text_body: params.inspect)
+
   'thanks'
 end
