44ade4d137aa1aac08e74fd1493049ae9c02630a
Forrest Ye <afu@forresty.com>
2014-07-25 16:59:19 +0800

    clean up with ayi

--- public/assets/all.js
+++ public/assets/all.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-	$(".js-float-label-wrapper").FloatLabel();
+  $(".js-float-label-wrapper").FloatLabel();
 
-	$(".js-float-option-wrapper").FloatSelect();
+  $(".js-float-option-wrapper").FloatSelect();
 
-	$(document).on("keypress", function(e) {
-		if(e.keyCode == 13) {
-			e.preventDefault();
-		}
-	});
+  $(document).on("keypress", function(e) {
+    if(e.keyCode == 13) {
+      e.preventDefault();
+    }
+  });
 
-	$("input, select").focus(function() {
-		$(this).parent(".js-float-label-wrapper ,js-float-option-wrapper").removeClass("error");
-	});
+  $("input, select").focus(function() {
+    $(this).parent(".js-float-label-wrapper ,js-float-option-wrapper").removeClass("error");
+  });
 });
--- public/assets/jquery.FloatOption.js
+++ public/assets/jquery.FloatOption.js
@@ -1,56 +1,56 @@
-(function( $ ){
+(function( $ ) {
 
-	$.fn.FloatSelect = function( options ){
+  $.fn.FloatSelect = function( options ) {
 
-		var defaults = {
-		
-		},
-			settings = $.extend({}, defaults, options);
+    var defaults = {
 
-		return this.each(function(){
-			var element = $(this),
-				label = element.find("label"),
-				select = element.find("select");
-				options = element.find("option");
+    },
+      settings = $.extend({}, defaults, options);
 
-			var optionsValue = $("<div class='js-float-select-status'></div>").prependTo(element);
-			optionsValue.text(label.text());
+    return this.each(function() {
+      var element = $(this),
+        label = element.find("label"),
+        select = element.find("select");
+        options = element.find("option");
 
-			var optionsContainer = $("<div class='float-option-container'></div>").appendTo(element).hide();
+      var optionsValue = $("<div class='js-float-select-status'></div>").prependTo(element);
+      optionsValue.text(label.text());
 
-			element.click(function() {
-				select.focus();
-			})
+      var optionsContainer = $("<div class='float-option-container'></div>").appendTo(element).hide();
 
-			select.change(function(){
-				optionsValue.text($("select option:selected").text());
-				element.addClass("populated");
-			})
+      element.click(function() {
+        select.focus();
+      })
 
-			select.focus(function(){
-				element.addClass("focus");
-				if (select[0].selectedIndex == 0) {
-					optionsValue.text(options.eq(0).text());
-				};
-			})
+      select.change(function() {
+        optionsValue.text($("select option:selected").text());
+        element.addClass("populated");
+      })
 
-			select.blur(function(){
-				element.removeClass("focus");
-				if (select[0].selectedIndex == 0) {
-					optionsValue.text(label.text());
-				};
-			})
+      select.focus(function() {
+        element.addClass("focus");
+        if (select[0].selectedIndex == 0) {
+          optionsValue.text(options.eq(0).text());
+        };
+      })
 
-			select.on("focus mouseenter",function(){
-				optionsValue.css({"border-color":"#ccc"});
-			})
+      select.blur(function() {
+        element.removeClass("focus");
+        if (select[0].selectedIndex == 0) {
+          optionsValue.text(label.text());
+        };
+      })
 
-			select.on("blur mouseleave",function(){
-				optionsValue.removeAttr("style");
-			})
+      select.on("focus mouseenter",function() {
+        optionsValue.css({"border-color":"#ccc"});
+      })
 
-		});
+      select.on("blur mouseleave",function() {
+        optionsValue.removeAttr("style");
+      })
 
-	};
+    });
+
+  };
 
 })( jQuery );
