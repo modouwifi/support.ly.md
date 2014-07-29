bc54ea0b04d1c254b8026a6af5c94819606d35d0
Forrest Ye <afu@forresty.com>
2014-07-29 17:04:27 +0800

    update views

--- public/assets/all.js
+++ public/assets/all.js
@@ -11,5 +11,29 @@ $(document).ready(function() {
 
   $("input, select").focus(function() {
     $(this).parent(".js-float-label-wrapper ,js-float-option-wrapper").removeClass("error");
-  });
-});
+  })
+
+  $("select").change(function() {
+    if ($(this)[0].selectedIndex == 0) {
+      $(".reason-description").hide(0);
+    } else if($(this)[0].selectedIndex == 1) {
+      $(".reason-description").show(0);
+      $(".reason-description").children().hide(0);
+      $(".reason-description-refund").show(0);
+    } else if($(this)[0].selectedIndex == 2) {
+      $(".reason-description").show(0);
+      $(".reason-description").children().hide(0);
+      $(".reason-description-return").show(0);
+    } else if($(this)[0].selectedIndex == 3) {
+      $(".reason-description").show(0);
+      $(".reason-description").children().hide(0);
+      $(".reason-description-exchange").show(0);
+    } else if($(this)[0].selectedIndex == 4) {
+      $(".reason-description").show(0);
+      $(".reason-description").children().hide(0);
+      $(".reason-description-repair").show(0);
+    } else if($(this)[0].selectedIndex == 5) {
+      $(".reason-description").hide(0);
+    }
+  })
+})
