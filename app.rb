fbabd0fdee228872671c2d83454b9f43402493f1
Forrest Ye <afu@forresty.com>
2014-07-29 17:16:08 +0800

    form names

--- app.rb
+++ app.rb
@@ -6,18 +6,23 @@ configure :production do
   require "newrelic_rpm"
 end
 
+helpers Sinatra::Param
+
 get '/' do
   erb :template
 end
 
 post '/support' do
   api_key = ENV["POSTMARK_API_KEY"]
-  client = Postmark::ApiClient.new(api_key)
 
-  client.deliver(from: 'cs@mochui.net',
-                 to: 'afu@forresty.com',
-                 subject: "support request from #{params['name']}",
-                 text_body: params.inspect)
+  if api_key
+    client = Postmark::ApiClient.new(api_key)
+
+    client.deliver(from: 'cs@mochui.net',
+                   to: 'afu@forresty.com',
+                   subject: "support request from #{params['name']}",
+                   text_body: params.inspect)
+  end
 
   'thanks'
 end
