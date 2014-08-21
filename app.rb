d9d241d3fd5f45f93efb3cf40a15611eb62ea846
Forrest Ye <afu@forresty.com>
2014-08-21 13:39:54 +0800

    use DAIKE_CUSTOM_HEADER env var

--- app.rb
+++ app.rb
@@ -94,7 +94,7 @@ post '/support' do
 
     client.deliver(from:        "cs@mochui.net",
                    to:          "cs@mochui.net",
-                   headers:     { 'Name' => 'X-Modouwifi-From', 'Value' => ticket.user_email },
+                   headers:     { 'Name' => ENV['DAIKE_CUSTOM_HEADER'], 'Value' => ticket.user_email },
                    subject:     "support request from #{ticket.user_name}",
                    text_body:   ticket.to_human_readable_text)
   end
